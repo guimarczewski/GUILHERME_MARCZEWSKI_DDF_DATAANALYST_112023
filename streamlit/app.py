96e3e18a661850a5bddbf4319f992c1ca5f6d086
guimarczewski <guimarczewski@gmail.com>
2023-11-25 18:34:41 +0000

    changed model

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import spacy
 
-models = ["en_core_web_md"]
+models = ["en_core_web_sm"]
 
 # Carregar dados do arquivo CSV hospedado no GitHub
 @st.cache
@@ -12,7 +12,7 @@ def load_data(file_url):
 
 # Função para calcular a similaridade entre dois textos usando spaCy
 def calculate_similarity(text1, text2):
-    nlp = spacy.load("en_core_web_md")
+    nlp = spacy.load("en_core_web_sm")
     doc1 = nlp(text1)
     doc2 = nlp(text2)
     similarity = doc1.similarity(doc2)
