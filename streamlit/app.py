d6d61b2b163d02093d1257b79cbd6397e3c35e58
guimarczewski <guimarczewski@gmail.com>
2023-11-25 21:18:25 +0000

    pt to en

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,7 +4,7 @@ import spacy
 
 models = ["en_core_web_sm"]
 
-# Carregar dados do arquivo CSV hospedado no GitHub
+# Load CSV file hosted on GitHub
 @st.cache
 def load_data(file_url):
     data = pd.read_csv(file_url, sep=';')
@@ -12,7 +12,7 @@ def load_data(file_url):
     data = data[data['category'].notnull()]
     return data
 
-# Função para calcular a similaridade entre dois textos usando spaCy
+# Function to calculate the similarity between two products with spaCy
 def calculate_similarity(text1, text2):
     nlp = spacy.load("en_core_web_sm")
     doc1 = nlp(text1)
@@ -20,40 +20,41 @@ def calculate_similarity(text1, text2):
     similarity = doc1.similarity(doc2)
     return similarity
 
-# Função principal do aplicativo
+# App
 def main():
     st.title("Similaridade de Produtos")
 
-    # URL do arquivo CSV no seu repositório do GitHub
+    # URL of the CSV file in your GitHub repository
     file_url = "streamlit/product-search-corpus-final.csv"
 
-    # Carregar dados
+    # Load data
     data = load_data(file_url)
 
-    # Filtros para selecionar dois produtos
-    product1_category = st.selectbox("Selecione a categoria do Produto 1", data["category"].unique())
-    product1_title = st.selectbox("Selecione o título do Produto 1", data[data["category"] == product1_category]["title"].unique())
+    # Filters to select two products
+    product1_category = st.selectbox("Select the Product category 1", data["category"].unique())
+    product1_title = st.selectbox("Select the Product title 1", data[data["category"] == product1_category]["title"].unique())
 
-    product2_category = st.selectbox("Selecione a categoria do Produto 2", data["category"].unique())
-    product2_title = st.selectbox("Selecione o título do Produto 2", data[data["category"] == product2_category]["title"].unique())
+    product2_category = st.selectbox("Select the Product category 2", data["category"].unique())
+    product2_title = st.selectbox("Select the Product title 2", data[data["category"] == product2_category]["title"].unique())
 
-    # Obter dados dos produtos selecionados
+    # Get data for selected products
     product1_data = data[(data["category"] == product1_category) & (data["title"] == product1_title)].iloc[0]
     product2_data = data[(data["category"] == product2_category) & (data["title"] == product2_title)].iloc[0]
 
-    # Exibir informações dos produtos
-    st.subheader("Produto 1:")
+    # View product information
+    st.subheader("Product 1:")
     st.write(product1_data)
 
-    st.subheader("Produto 2:")
+    st.subheader("Product 2:")
     st.write(product2_data)
 
-    # Calcular e exibir a similaridade entre os textos dos produtos sem as chaves e aspas
+    # Calculate and display similarity between product texts without braces and quotes
     product1_data['features'] = product1_data['features'].replace('{', '').replace('}', '').replace('"', '')
     product2_data['features'] = product2_data['features'].replace('{', '').replace('}', '').replace('"', '')
+
     similarity_score = calculate_similarity((product1_data["features"] + '-' + product1_data["category"]), (product2_data["features"] + '-'  + product2_data["category"]))
-    st.subheader("Similaridade entre os produtos:")
-    st.write(f"A similaridade entre os produtos é: {similarity_score:.2%}")
+    st.subheader("Similarity between products:")
+    st.write(f"The similarity between products is: {similarity_score:.2%}")
 
 if __name__ == "__main__":
     main()
