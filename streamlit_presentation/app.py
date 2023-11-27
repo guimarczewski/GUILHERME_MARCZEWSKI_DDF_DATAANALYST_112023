a00fc1a13c4fccbccc90443ef64feda7d08c1a7b
guimarczewski <guimarczewski@gmail.com>
2023-11-27 00:59:06 +0000

    update

--- streamlit_presentation/app.py
+++ streamlit_presentation/app.py
@@ -157,10 +157,10 @@ def app():
             replace_text({"{product_name}": product_name}, slide_0)
 
             st.text(strategies_dict)
-            cold_1 = str(strategies_dict["cold_1"])
-            cold_2 = str(strategies_dict[1])
-            remarketing = str(strategies_dict[2])
-            abandoned = str(strategies_dict[3])
+            cold_1 = str(strategies_dict[0][1])
+            cold_2 = str(strategies_dict[1][1])
+            remarketing = str(strategies_dict[2][1])
+            abandoned = str(strategies_dict[3][1])
             
             slide_1 = presentation.slides[1]
             replace_text({"{c}": product_name}, slide_1)
